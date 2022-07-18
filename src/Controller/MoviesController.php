3fc0ebe42cf89296c085952523fc68905c5a6976
Gavril Hasmasan <gavriel.hasmasan@global-fashion-group.com>
2022-07-18 13:13:40 +0300

    __construct di

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Actor;
 use App\Entity\Movie;
 use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,16 +15,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
 
-    #[Route('/movies', name: 'movies')]
-    public function index(EntityManagerInterface $em): Response
+    public function __construct(private readonly EntityManagerInterface $em)
     {
-        $repository = $em->getRepository(Movie::class);
-        $movies = $repository->findAll();
+    }
 
-        dd($movies);
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->findAll();
 
         return $this->render('index.html.twig', [
             'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/second', name: 'second')]
+    public function second(): Response
+    {
+        $repository = $this->em->getRepository(Actor::class);
+        $actors = $repository->findAll();
+
+        dd($actors);
+
+        return $this->render('index.html.twig', [
+            'actors' => $actors,
         ]);
     }
 }
